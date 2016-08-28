e59021381609ab25887e3999d5958a65be1f382f
Rogelio Negrete <rogelio_negrete@live.com>
2016-08-28 09:56:38 -0700

    ITS WORKING!

--- Source.cpp
+++ Source.cpp
@@ -1,147 +1,92 @@
+/* CPSC 323 - Assignment #1
+ * Names: Rogelio Negrete, Ivan Vu, Angelo Salac
+ */
+
 #include <iostream>
 #include <string>
 #include <unordered_map>
-#include <deque>
+#include <stack>
 
 using namespace std;
 
-
-deque<char> split_into_deque(string input)
-{
-	deque<char> charlist; // deque to work with 
-
-	// iterate through string and push chars to queue
-	for (auto i : input)
-	{
-		if (!isspace(i))
-			charlist.push_back(i);
-	}
-
-	return charlist;
-}
-
 int eval_operation(char operation, int operand1, int operand2)
 {
-	// operation -= 57; 
-	// getting some stupid fucking error when passing in 'operation' or 'charlist[i]'
-	// the ascii value gets set to 99 (C), the letter, instead of being, 48 (*), the star symbol
-	// gonna try this on my linux desktop to see if its just VStudio being dumb...
-	//other than that it should work -___-
 	if (operation == '+')
-	{
-		cout << "\tPerforming Addition!";
 		return operand1 + operand2;
-	}
 	else if (operation == '-')
-	{
-		cout << "\tPerforming Subtraction!";
 		return operand1 - operand2;
-	}
 	else if (operation == '*')
-	{
-		cout << "\tPerforming Multiplication!";
 		return operand1 * operand2;
-	}
 	else if (operation == '/')
-	{
-		cout << "\tPerforming Division!";
 		return operand1 / operand2;
-	}
-	else {
+	else 
+	{
 		cout << "Unexpected Error \n";
 		return -1;
 	}
 }
 
-void process_deque(deque<char> charlist, unordered_map<char, char> dict)
+void process_expression(string expression)
 {
-	for (int i = 0; i < charlist.size(); i++)
+	unordered_map<char, int> dict;
+	stack<int> resultlist;
+
+	for (int i = 0; i < expression.length(); i++)
 	{
-		cout << "\ncurrent item: ";
-		cout << charlist[i] << endl;
-		cout << "i value: " << i << endl;
-
-		// found i to be an operation
-		if (charlist[i] == '+' || charlist[i] == '-' || charlist[i] == '/' || charlist[i] == '*')
+		// check for operations
+		if (expression[i] == '+' || expression[i] == '-' || expression[i] == '*' || expression[i] == '/')
 		{
-			//grab the 2 top digits
-			int operand1 = (charlist.front() - '0');
-			charlist.pop_front();
+			int operand1 = resultlist.top(); resultlist.pop();
+			int operand2 = resultlist.top(); resultlist.pop();
 
-			// (char_value - '0') is a dirty trick to convert from type<char> -> type<int> implicitly
-			int operand2 = (charlist.front() - '0');
-			charlist.pop_front();
-			
-			cout << "\n-----------------\n" << "Performing operation:" << endl;
-			cout << "operand1: " << operand1 << endl;
-			cout << "operand2: " << operand2 << endl;
-
-			char op = charlist[i];
-			cout << "op: ";
-			cout << op << endl;
-
-
-			// we eval the 2 numbers and convert back to type<char> implicitly
-			int result = eval_operation(op, operand1, operand2);
-			cout << "eval result: ";
-			cout << result << endl;
-
-			// remove operation from deque
-			charlist.pop_front();
-			
-			charlist.push_front(result);
-
-			i = 1; //reset i? errors with indexing...
+			//push computed value back onto stack
+			resultlist.push(eval_operation(expression[i], operand1, operand2));
 
 		}
-		else // found i to be a char[a-z]
+		// found to be a char[a-z] ... ignore the $ and spaces
+		else if (expression[i] >= 'a' && expression[i] <= 'z')
 		{
 			// check if current element is NOT in our dict
-			if (dict.count(charlist[i]) == 0)
+			if (dict.count(expression[i]) == 0)
 			{
 				char char_value;
-				//Prompt user for the value of "i"
-				cout << "Enter the value of " << charlist[i] << ": " << endl;
+				int int_value;
+				// Prompt user for the value of "expression[i]"
+				cout << "\nEnter the value of " << expression[i] << ": ";
 				cin >> char_value;
+				int_value = char_value - '0'; 
 
-				cout << "Before replacing: " << charlist[i] << endl;
-				// update/replace i to its "char digit"
-				charlist[i] = char_value;
-
-				cout << "After replacing: " << charlist[i] << endl;
+				// push int_value to the stack 
+				resultlist.push(int_value);
 
 				// store into our dict for future use
-				dict.emplace(i, char_value);
+				dict.emplace(expression[i], int_value);
 			}
 			else // current element is in our dict
 			{
-				// update/replace i to its "char digit"
-				charlist[i] = dict.find(charlist[i])->second;
+				// find our match in the dict
+				int int_value = dict.find(expression[i])->second;
+
+				// push int_value to the stack 
+				resultlist.push(int_value);
 			}
 		}
-			
+
 	}
+
+	// there should only be one value left in the stack which is the final value
+	cout << "Final Value: " << resultlist.top() << endl;
+
 }
 
-
 int main() {
-
-	unordered_map<char, char> dict;
-	deque<char> charlist;
 	string user_input;
 	
 	// Get user postfix expression
 	cout << "Enter a postfix expression with a $ at the end: " << endl;
 	getline(cin, user_input);
 
-	user_input.pop_back(); //remove $... we don't need it, lol
-	
-	charlist = split_into_deque(user_input);
-	process_deque(charlist, dict);
-
-	//there will only be one elem in our charlist which is our result
-
-	cout << "Final Value = " << charlist.front();
+	process_expression(user_input);
 
 	return 0;
 }
